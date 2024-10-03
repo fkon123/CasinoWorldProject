2c00dd9e0d9e5b733aea36b7a1c9f6c7c38bcfaf
fil123 <fil13kon@gmail.com>
2024-10-03 16:37:32 +0300

    fix typo & add theme files

--- src/plugins/casino-world-admin/casino-world-admin.php
+++ src/plugins/casino-world-admin/casino-world-admin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Casino World Admin
-Description: Handling custom DB tables for casino menu & casino entities
+Description: Handling custom DB tables for casino menus & casino entities
 Version: 1.0.0
 Author: Fil123
 */
@@ -10,10 +10,10 @@ function create_custom_tables() {
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
 
-    $casino_menu_table = $wpdb->prefix . 'casino_menu';
+    $casino_menus_table = $wpdb->prefix . 'casino_menus';
     $casino_entities_table = $wpdb->prefix . 'casino_entities';
 
-    $casino_menu_table_script = "CREATE TABLE IF NOT EXISTS `$casino_menu_table` (
+    $casino_menus_table_script = "CREATE TABLE IF NOT EXISTS `$casino_menus_table` (
         id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(255) NOT NULL,
         url VARCHAR(255) NOT NULL,
@@ -35,7 +35,7 @@ function create_custom_tables() {
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
     //TODO dbDelta can take array of queries but it didn't work for me
-    dbDelta($casino_menu_table_script);
+    dbDelta($casino_menus_table_script);
     dbDelta($casino_entities_table_script);
 }
 
