2a30e3ebadddeee977f230263cabc7c5673e30dd
fil123 <fil13kon@gmail.com>
2024-10-03 14:31:12 +0300

    create custom tables fixed

--- src/plugins/casino-world-admin/casino-world-admin.php
+++ src/plugins/casino-world-admin/casino-world-admin.php
@@ -10,19 +10,18 @@ function create_custom_tables() {
 
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
-    $create_script_list = array();
 
     $casino_menu_table = $wpdb->prefix . 'casino_menu';
     $casino_entities_table = $wpdb->prefix . 'casino_entities';
 
-    $casino_menu_table_script = "CREATE TABLE IF NOT EXISTS $casino_menu (
+    $casino_menu_table_script = "CREATE TABLE IF NOT EXISTS `$casino_menu_table` (
         id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(255) NOT NULL,
         url VARCHAR(255) NOT NULL,
         PRIMARY KEY (id)
     ) $charset_collate;";
 
-    $casino_entities_table_script = "CREATE TABLE IF NOT EXISTS $casino_entities (
+    $casino_entities_table_script = "CREATE TABLE IF NOT EXISTS `$casino_entities_table` (
         id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(255) NOT NULL,
         rating FLOAT NOT NULL,
@@ -34,8 +33,7 @@ function create_custom_tables() {
         PRIMARY KEY (id)
     ) $charset_collate;";
 
-    $create_script_list[] = $casino_menu_table_script;
-    $create_script_list[] = $casino_entities_table_script;
+    $create_script_list = array($casino_menu_table_script, $casino_entities_table_script);
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta($create_script_list);
