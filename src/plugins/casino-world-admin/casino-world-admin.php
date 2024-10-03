d2a2dd1baa4c85f28f1fe9f7d8c9fc6c76a11691
fil123 <fil13kon@gmail.com>
2024-10-03 13:51:17 +0300

    custom plugin for creating tables

---
+++ src/plugins/casino-world-admin/casino-world-admin.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Casino World Admin
+Description: Handling custom DB tables for casino menu & casino entities
+Version: 1.0.0
+Author: Fil123
+*/
+
+function create_custom_tables() {
+
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    $create_script_list = array();
+
+    $casino_menu_table = $wpdb->prefix . 'casino_menu';
+    $casino_entities_table = $wpdb->prefix . 'casino_entities';
+
+    $casino_menu_table_script = "CREATE TABLE IF NOT EXISTS $casino_menu (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        url VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    $casino_entities_table_script = "CREATE TABLE IF NOT EXISTS $casino_entities (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        rating FLOAT NOT NULL,
+        code VARCHAR(255) NOT NULL,
+        bonus VARCHAR(255) NOT NULL,
+        url VARCHAR(255) NOT NULL,
+        imageurl VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    $create_script_list[] = $casino_menu_table_script;
+    $create_script_list[] = $casino_entities_table_script;
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta($create_script_list);
+}
+
+register_activation_hook(__FILE__, 'create_custom_tables');
