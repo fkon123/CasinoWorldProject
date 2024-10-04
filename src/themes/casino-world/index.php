7efe3f5a391829e0f96579ad632a1cb9bdef8f0d
fil123 <fil13kon@gmail.com>
2024-10-04 15:07:13 +0300

    get casions from db to index

--- src/themes/casino-world/index.php
+++ src/themes/casino-world/index.php
@@ -1,4 +1,21 @@
 <?php get_header(); ?>
 
+<div class="casino-listings">
+    <?php
+    global $wpdb;
+    $casinos = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}casino_entities");
+    foreach ($casinos as $casino) {
+        ?>
+        <div>
+            <img src="<?php echo esc_url($casino->image_url); ?>" alt="<?php echo esc_attr($casino->name); ?>">
+            <h2><?php echo esc_html($casino->name); ?></h2>
+            <p><?php echo esc_html($casino->bonus); ?></p>
+            <p><?php echo esc_html($casino->description); ?></p>
+            <a href="<?php echo esc_url($casino->url); ?>">Activate Bonus</a>
+        </div>
+        <?php
+    }
+    ?>
+</div>
 
 <?php get_footer(); ?>
