9c73101a963f147bdd8919cbfdda05bc9ee48819
fil123 <fil13kon@gmail.com>
2024-10-05 16:50:20 +0300

    fix nav dropdown

--- src/themes/casino-world/header.php
+++ src/themes/casino-world/header.php
@@ -8,23 +8,22 @@
 <body class="bg-purple-900" <?php body_class(); ?>>
 <header class="bg-purple-900 text-white py-4">
     <div class="container mx-auto flex justify-between items-center">
-
         <h1 class="text-3xl">Casino <span class="font-bold">World</span></h1>
 
         <nav>
             <ul class="flex space-x-6 text-lg">
-               
                 <li class="relative group">
-                <a href="#" class="inline-block">Online Casinos</a>
-                <ul class="absolute left-0 hidden group-hover:block bg-white text-black rounded shadow-lg mt-2 w-48">
-                    <?php
-                    global $wpdb;
-                    $casino_menus = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}casino_entities");
-                    foreach ($casino_menus as $casino_menu) {
-                        echo '<li><a href="' . esc_url($casino_menu->url) . '" class="block px-4 py-2 hover:bg-gray-200">' . esc_html($casino_menu->name) . '</a></li>';
-                    }
-                    ?>
-                </ul>
+                    <a href="#" class="inline-block">Online Casinos</a>
+
+                    <ul class="absolute left-0 top-full hidden group-hover:block bg-white text-black rounded shadow-lg w-48 z-20">
+                        <?php
+                        global $wpdb;
+                        $casino_menus = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}casino_entities");
+                        foreach ($casino_menus as $casino_menu) {
+                            echo '<li><a href="' . esc_url($casino_menu->url) . '" class="block px-4 py-2 hover:bg-gray-200">' . esc_html($casino_menu->name) . '</a></li>';
+                        }
+                        ?>
+                    </ul>
                 </li>
                 <li><a href="#">Slots</a></li>
                 <li><a href="#">Software</a></li>
@@ -35,7 +34,6 @@
                 <li><a href="#">Live Casino</a></li>
                 <li><a href="#">Poker</a></li>
                 <li><a href="#">Extra</a></li>
-  
             </ul>
         </nav>
     </div>
