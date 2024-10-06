b4dceb69c2823a842b86e0e157e88acaa9361b7d
fil123 <fil13kon@gmail.com>
2024-10-06 14:01:19 +0300

    import given css

--- src/themes/casino-world/tailwind.config.js
+++ src/themes/casino-world/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     './**/*.php',      
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--unnamed-color-3bdec8)',
+        secondary: 'var(--unnamed-color-7d33ff)',
+        highlight: 'var(--unnamed-color-ffba00)',
+        danger: 'var(--unnamed-color-ff4757)',
+        success: 'var(--unnamed-color-45c93a)',
+      },
+      fontFamily: {
+        montserrat: ['var(--unnamed-font-family-montserrat)', 'sans-serif'],
+      },
+      fontSize: {
+        '14': 'var(--unnamed-font-size-14)',
+        '22': 'var(--unnamed-font-size-22)',
+        '24': 'var(--unnamed-font-size-24)',
+        '28': 'var(--unnamed-font-size-28)',
+      },
+    },
   },
   plugins: [],
 }
